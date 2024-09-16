a4b6c604168a9b4d51df2c724a70d4874fb1d332
Brayan Baquero <78622205+babaquero07@users.noreply.github.com>
2024-09-15 20:30:15 -0500

    Configuración de rutas principales y secundarias

---
+++ src/auth/routes/AuthRoutes.jsx
@@ -0,0 +1,19 @@
+import { Navigate } from "react-router-dom";
+
+import { LoginPage, RegisterPage } from "../pages";
+
+export const AuthRoutes = [
+  {
+    index: true,
+    path: "login",
+    element: <LoginPage />,
+  },
+  {
+    path: "register",
+    element: <RegisterPage />,
+  },
+  {
+    path: "auth/*",
+    element: <Navigate to="/auth/login" />,
+  },
+];
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
-import { JournalApp } from "./JournalApp";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { AppRouter } from "./router/AppRouter";
+
+const router = createBrowserRouter(AppRouter);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <JournalApp />
+    <RouterProvider router={router} />
   </StrictMode>
 );
