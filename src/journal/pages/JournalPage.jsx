be9a929d79180ba4ed5a99d7e545b37bfd26d9ae
Brayan Baquero <78622205+babaquero07@users.noreply.github.com>
2024-09-16 16:19:00 -0500

    Floating Button

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,5 +1,7 @@
+import { Fab } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
 import { NoteView, NothingSelectedView } from "../views";
+import { Add } from "@mui/icons-material";
 
 export const JournalPage = () => {
   return (
@@ -7,6 +9,18 @@ export const JournalPage = () => {
       {/* <NothingSelectedView /> */}
 
       <NoteView />
+
+      {/* Floating Action Button */}
+      <Fab
+        color="primary"
+        sx={{
+          position: "fixed",
+          right: 50,
+          bottom: 50,
+        }}
+      >
+        <Add />
+      </Fab>
     </JournalLayout>
   );
 };
