031d895a9ca0c701c760b1eab25ac8c03413857f
Brayan Baquero <78622205+babaquero07@users.noreply.github.com>
2024-09-16 12:30:49 -0500

    JournalLayout y JournalPage

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,5 +1,14 @@
 import { Typography } from "@mui/material";
+import { JournalLayout } from "../layout/JournalLayout";
 
 export const JournalPage = () => {
-  return <Typography variant="h1">Journal Page</Typography>;
+  return (
+    <JournalLayout>
+      <Typography variant="body1">
+        Cupidatat qui aliqua voluptate consectetur. Eiusmod id reprehenderit
+        laborum esse pariatur. Adipisicing nisi excepteur labore consectetur
+        velit sint eu minim cillum ut qui esse sint.
+      </Typography>
+    </JournalLayout>
+  );
 };
