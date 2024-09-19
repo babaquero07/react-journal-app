6b29d9e3179c09b58db74633d6d9b24fad6df24a
Brayan Baquero <78622205+babaquero07@users.noreply.github.com>
2024-09-19 13:07:05 -0500

    Configurar el AuthSlice

---
+++ src/store/auth/authSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  status: "checking", // 'checking', 'authenticated', 'unauthenticated'
+  uuid: null,
+  email: null,
+  displayName: null,
+  photoURL: null,
+  errorMessage: null,
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    login: (state, action) => {},
+    logout: (state, payload) => {},
+    checkingCredentials: (state) => {},
+  },
+});
+
+export const { login, logout, checkingCredentials } = authSlice.actions;
