2aeb3334dc409657aa87f1e19f4459ff92e34e9b
Brayan Baquero <78622205+babaquero07@users.noreply.github.com>
2024-09-20 18:21:24 -0500

    Custom Hook para autenticación

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -5,14 +5,10 @@ import {
 } from "react-router-dom";
 import { AuthRoutes } from "../auth/routes/AuthRoutes";
 import { JournalRoutes } from "../journal/routes/JournalRoutes";
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import { onAuthStateChanged } from "firebase/auth";
-import { FirebaseAuth } from "../firebase/config";
-import { login, logout } from "../store/auth";
 import { CheckingAuth } from "../ui";
 import { AuthRouter } from "../auth/routes/AuthRouter";
 import { JournalRouter } from "../journal/routes/JournalRouter";
+import { useCheckAuth } from "../hooks";
 
 const router = createBrowserRouter([
   {
@@ -32,19 +28,7 @@ const router = createBrowserRouter([
 ]);
 
 export const AppRouter = () => {
-  const { status } = useSelector((state) => state.auth);
-
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    onAuthStateChanged(FirebaseAuth, async (user) => {
-      if (!user) return dispatch(logout());
-
-      const { uid, email, displayName: fullName, photoURL } = user;
-
-      dispatch(login({ uid, email, fullName, photoURL }));
-    });
-  }, []);
+  const status = useCheckAuth();
 
   if (status === "checking") return <CheckingAuth />;
 
