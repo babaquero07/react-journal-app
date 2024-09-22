88c810bd00d08934c89b67eccdf0a749ac4ac279
Brayan Baquero <78622205+babaquero07@users.noreply.github.com>
2024-09-22 18:07:41 -0500

    Refactor ImageGallery component to handle empty images prop

--- src/journal/components/ImageGallery.jsx
+++ src/journal/components/ImageGallery.jsx
@@ -3,20 +3,24 @@ import PropTypes from "prop-types";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 
-export const ImageGallery = ({ images }) => {
+export const ImageGallery = ({ images = [] }) => {
   return (
-    <ImageList sx={{ width: "100%", height: 500 }} cols={4} rowHeight={200}>
-      {images?.map((imageUrl) => (
-        <ImageListItem key={imageUrl}>
-          <img
-            srcSet={`${imageUrl}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-            src={`${imageUrl}?w=164&h=164&fit=crop&auto=format`}
-            alt="note image"
-            loading="lazy"
-          />
-        </ImageListItem>
-      ))}
-    </ImageList>
+    <>
+      {images?.length > 0 && (
+        <ImageList sx={{ width: "100%", height: 500 }} cols={4} rowHeight={200}>
+          {images?.map((imageUrl) => (
+            <ImageListItem key={imageUrl}>
+              <img
+                srcSet={`${imageUrl}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                src={`${imageUrl}?w=164&h=164&fit=crop&auto=format`}
+                alt="note image"
+                loading="lazy"
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
+      )}
+    </>
   );
 };
 
